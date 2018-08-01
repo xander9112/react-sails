499d9a17c871f2ca1ebe21259daabf26e5b0a52c
AlexZander <xander91@mail.ru>
2018-08-01 22:37:14 +0300

    Add project

---
+++ assets/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log('hey there!!');
+
+const App = () => {
+    return (<div> Simple Sails-React stater </div>);
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
